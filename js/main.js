ce809a567e67fa1af564b39ae8608bce300a5a32
Patrick Finazzo (music) <patrick_finazzo_music@Patricks-MacBook-Pro.local>
2017-08-26 19:16:11 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cards.push('cardOne');
+cardsInPlay.push('cardOne');
+
+cards.push('cardTwo');
+cardsInPlay.push('cardTwo');
+
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+console.log();
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} 
+    else {
+		alert("Sorry, try again.")
+	}
+}
+
+
+
+
+
+
+
+
 
