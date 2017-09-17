bc432072aa109514ff8d3ac319ae6fdc85a349fc
Patrick Finazzo (music) <patrick_finazzo_music@Patricks-MacBook-Pro.local>
2017-09-16 22:06:49 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -10,32 +29,35 @@ var checkForMatch = function() {
     }
 };
 
-
-var flipCard = function(cardId) {
-
-/* var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-unnecesary code^^ */
-
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
-
+var flipCard = function() {
+var cardId = this.getAttribute('data-id');
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cardImage);
+console.log(suit);
+this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 	checkForMatch();
-
-
 }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.innerHTML = 'img';
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id',i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+ }
+};
+
+createBoard();
+
+
+
+
+
 
 
 
